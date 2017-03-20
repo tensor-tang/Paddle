e003f6c66c78e614400feeaaec7e6ed25278aa6a
tensor-tang <jian.j.tang@intel.com>
2017-03-20 09:55:09 +0800

    add vgg16

--- demo/mkldnn_vgg/vgg.py
+++ demo/mkldnn_vgg/vgg.py
@@ -2,6 +2,7 @@
 from paddle.trainer_config_helpers import *
 
 batch_size = get_config_arg('batch_size', int, 64)
+layer_num = get_config_arg('layer_num', int, 16)
 is_predict = get_config_arg("is_predict", bool, False)
 is_test = get_config_arg("is_test", bool, False)
 use_dummy = get_config_arg("use_dummy", bool, False)
@@ -40,23 +41,11 @@ settings(
     regularization=L2Regularization(0.0005 * batch_size))
 
 #######################Network Configuration #############
-
 img = data_layer(name='image', size=data_size)
-def vgg_19_network(input_image, num_channels, num_classes=1000):
-    """
-    Same model from https://gist.github.com/ksimonyan/211839e770f7b538e2d8
-
-    :param num_classes:
-    :param input_image:
-    :type input_image: LayerOutput
-    :param num_channels:
-    :type num_channels: int
-    :return:
-    """
-
+def vgg_network(vgg_num=3):
     tmp = img_conv_group(
-        input=input_image,
-        num_channels=num_channels,
+        input=img,
+        num_channels=3,
         conv_padding=1,
         conv_num_filter=[64, 64],
         conv_filter_size=3,
@@ -75,19 +64,24 @@ def vgg_19_network(input_image, num_channels, num_classes=1000):
         pool_type=MaxPooling(),
         pool_size=2)
 
+    channels = []
+    for i in range(vgg_num):
+        channels.append(256)
     tmp = img_conv_group(
         input=tmp,
-        conv_num_filter=[256, 256, 256, 256],
+        conv_num_filter=channels,
         conv_padding=1,
         conv_filter_size=3,
         conv_act=ReluActivation(),
         pool_stride=2,
         pool_type=MaxPooling(),
         pool_size=2)
-
+    channels = []
+    for i in range(vgg_num):
+        channels.append(512)
     tmp = img_conv_group(
         input=tmp,
-        conv_num_filter=[512, 512, 512, 512],
+        conv_num_filter=channels,
         conv_padding=1,
         conv_filter_size=3,
         conv_act=ReluActivation(),
@@ -96,7 +90,7 @@ def vgg_19_network(input_image, num_channels, num_classes=1000):
         pool_size=2)
     tmp = img_conv_group(
         input=tmp,
-        conv_num_filter=[512, 512, 512, 512],
+        conv_num_filter=channels,
         conv_padding=1,
         conv_filter_size=3,
         conv_act=ReluActivation(),
@@ -118,11 +112,16 @@ def vgg_19_network(input_image, num_channels, num_classes=1000):
 
     return fc_layer(input=tmp, size=num_classes, act=SoftmaxActivation())
 
-predict = vgg_19_network(input_image=img, num_channels=3, num_classes=num_classes)
+if layer_num == 16:
+    output = vgg_network(3)
+elif layer_num == 19:
+    output = vgg_network(4)
+else:
+    print("Wrong layer number.")
 
 if not is_predict:
     lbl = data_layer(name="label", size=label_size)
     inputs(img, lbl)
-    outputs(classification_cost(name='loss', input=predict, label=lbl))
+    outputs(classification_cost(name='loss', input=output, label=lbl))
 else:
-    outputs(predict)
+    outputs(output)
