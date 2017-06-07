521921fd56b8c7795e6b0845fe8acad74e80c494
tensor-tang <jian.j.tang@intel.com>
2017-06-07 16:15:22 +0800

    use KMP setting separately topology.

--- demo/mkldnn/googlenet.sh
+++ demo/mkldnn/googlenet.sh
@@ -3,4 +3,7 @@ if [ $# -lt 1 ]; then
     echo "Input task: train/test/pretrain/time"
     exit 0
 fi
+
+export KMP_AFFINITY="verbose,granularity=fine,compact"
+
 ./run.sh googlenet $@
--- demo/mkldnn/vgg.sh
+++ demo/mkldnn/vgg.sh
@@ -3,4 +3,7 @@ if [ $# -lt 1 ]; then
     echo "Input task: train/test/pretrain/time"
     exit 0
 fi
+
+export KMP_AFFINITY="verbose,granularity=fine,compact,1,0"
+
 ./run.sh vgg $@
