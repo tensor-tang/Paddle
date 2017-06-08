77889623eb1116407bdbbb545f0af1399118bc53
tensor-tang <jian.j.tang@intel.com>
2017-06-08 21:56:51 +0800

    gpu and cpu benchmark use the same topologies

--- benchmark/gpu/run.sh
+++ benchmark/gpu/run.sh
@@ -1,153 +1,66 @@
 #!/bin/bash
 set -e
 
-unset OMP_NUM_THREADS MKL_NUM_THREADS
-num=$((`nproc`-2))
-use_num=$(($num>0?$num:1))
-export OMP_NUM_THREADS=$use_num
-export MKL_NUM_THREADS=$use_num
+# settings
+use_dummy=1
+use_mkldnn=0
+use_mkldnn_wgt=0
+version=
+use_gpu=1
+trainer_count=1
+cur_dir=$(cd "`dirname $0`"; pwd -P)
+log_prefix=$cur_dir
+dot_period=1
+log_period=25
+test_period=150
 
-function run() {
-    task=$1
-    topology=$2
-    layer_num=$3
-    bs=$4
-    cfg=${topology}.py
-    model=./models/${topology}_${layer_num}/
-    output=./models/${topology}_${layer_num}
-    thread=1
-    is_test=0
-    use_mkldnn=1
-    use_mkldnn_wgt=0
-    use_gpu=1
-    if [ $5 ]; then
-        thread=$5
-    fi
-    if [ $6 ]; then
-        use_gpu=$6
-    fi
-    if [ $task == "test" ]; then
-        is_test=1
-        use_mkldnn_wgt=0 # the models are trained by CPU yet, so compatible with CPU weight
-        if [ ! -d $model ]; then
-            echo "$model does not exist!"
-            exit 0
-        fi
-    fi
-    if [ $thread -gt 1 ]; then
-        use_mkldnn=0
-        unset OMP_NUM_THREADS MKL_NUM_THREADS
-        log=logs/"log_${task}_${topology}_${layer_num}_bs${bs}_thread${thread}.log"
-    else
-        log=logs/"log_${task}_${topology}_${layer_num}_bs${bs}_dummy.log"
-    fi
-    if [ -f $log ]; then
-        echo "remove old log $log"
-        rm -f $log
-    fi
-    if [ $use_gpu -eq 1 ] || [ $use_gpu == "True" ] ; then
-        unset OMP_NUM_THREADS MKL_NUM_THREADS
-        use_mkldnn=0
-    fi
-    if [ $use_mkldnn -eq 1 ] && [ $task == "train" ]; then
-        use_mkldnn_wgt=1
-    fi
-    args="layer_num=${layer_num},batch_size=${bs},use_dummy=1,\
-use_mkldnn=${use_mkldnn},use_mkldnn_wgt=${use_mkldnn_wgt},is_test=${is_test}"
+batch_sizes=(32 64 128 256 512 1024)
 
-    if [ $task == "time" ]; then
-        paddle train --job=$task \
-            --config=$cfg \
-            --use_gpu=$use_gpu \
-            --trainer_count=$thread \
-            --log_period=10 \
-            --test_period=100 \
-            --config_args=$args \
-            2>&1 | tee -a $log 2>&1 
-    elif [ $task == "test" ]; then # test
-        paddle train --job=$task \
-            --config=$cfg \
-            --use_gpu=$use_gpu \
-            --trainer_count=$thread \
-            --dot_period=1 \
-            --log_period=10 \
-            --init_model_path=$model \
-            --config_args=$args \
-            2>&1 | tee -a $log 2>&1 
-    else
-        paddle train --job=$task \
-            --config=$cfg \
-            --use_gpu=$use_gpu \
-            --trainer_count=$thread \
-            --dot_period=1 \
-            --log_period=5 \
-            --test_all_data_in_one_period=0 \
-            --num_passes=2 \
-            --save_dir=$output \
-            --config_args=$args \
-            2>&1 | tee -a $log 2>&1
-    fi
+
+function usage() {
+    echo "time_topology vgg/googlene/resnet version/layer_num"
+    echo "The input (version/layer_num) is version in GoogleNet(only support v1 yet),\
+ layer_num in VGG(16 or 19) and ResNet(50, 101 or 152)."
 }
 
-train_list="data/train.list"
-test_list="data/test.list"
-if [ ! -d "data" ]; then
-    mkdir -p data
-fi
-if [ ! -f $test_list ]; then
-    echo " " > $test_list
-fi
-if [ ! -f $train_list ]; then
-    echo " " > $train_list
-fi
+function time_topology() {
+    if [ $# -lt 2 ]; then
+        echo "At least 2 inputs!"
+        usage
+        exit 0
+    fi
+    topology=$1
+    version=$2
+    if [ $topology != "googlenet" ] && [ $topology != "vgg" ] \
+     && [ $topology != "resnet" ]; then
+        echo "unknown topology: ${topology}"
+        usage
+        exit 0
+    fi
+    echo "Start Timing ${topology}_${version} ......"
+    topology="./${topology}.sh"
+    for bs in ${batch_sizes[@]}; do
+        $topology time $bs $use_dummy $use_mkldnn $use_mkldnn_wgt \
+$version $use_gpu $trainer_count $log_prefix $dot_period $log_period $test_period
+        sleep 10s
+    done
+}
 
-if [ ! -d "logs" ]; then
-  mkdir logs
-fi
-
-### test
-# GoogleNet
-run test googlenet v1 2
-run test googlenet v1 8
-run test googlenet v1 16
-run test googlenet v1 32
-run test googlenet v1 64
-sleep 10s
-
-# VGG
-run test vgg 19 2
-run test vgg 19 8
-run test vgg 19 16
-run test vgg 19 32
-run test vgg 19 64
-sleep 10s
-
-# ResNet
-run test resnet 50 2
-run test resnet 50 8
-run test resnet 50 16
-run test resnet 50 32
-run test resnet 50 64
-sleep 10s
-
-### time
-# GoogleNet
-run time googlenet v1 32
-run time googlenet v1 64
-run time googlenet v1 128
-run time googlenet v1 256
-sleep 10s
+# main
+clear
+echo "-------------------------------------------------------------------------------"
+cd ../../demo/mkldnn
 
 # VGG
-run time vgg 19 32
-run time vgg 19 64
-run time vgg 19 128
-run time vgg 19 256
-sleep 10s
+time_topology vgg 19
+time_topology vgg 16
+
+# GoogleNet v1
+time_topology googlenet v1
 
 # ResNet
-run time resnet 50 32
-run time resnet 50 64
-run time resnet 50 128
-run time resnet 50 256
+time_topology resnet 50
+time_topology resnet 101
+time_topology resnet 152
 
+cd -
